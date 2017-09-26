29e1a271907c223270ca5028b49e184e7c1f889f
Samiksha Chandak <schandak@tavisca.com>
2017-09-26 12:55:23 +0530

    added implementation and test

---
+++ PackageLog/ESClient.cs
@@ -0,0 +1,22 @@
+﻿using Elasticsearch.Net;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageLog
+{
+    public class EsClient
+    {
+        public ElasticClient Client { get; set; }
+
+        public EsClient()
+        {
+            var pool = new SingleNodeConnectionPool(new Uri("http://localhost:9200"));
+            var connectionSettings = new ConnectionSettings(pool);
+            Client = new ElasticClient(connectionSettings);
+        }
+    }
+}
